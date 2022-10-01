a4176081b36751478f3dc140f30fe8d80b8a1bdd
Puneet Kittur <puneet.kittur@gmail.com>
2022-10-01 10:34:14 +0530

    Completed exercises 3.9 - 3.11, Phonebook backend step9-10, fullstack

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ app.use(morgan(":method :url :status :response-time[digits] :body"));
 
 app.use(cors());
 
+app.use(express.static("build"));
+
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!<h1>");
 });
