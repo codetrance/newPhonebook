466e20a8415a59cdbf5dbec2a9410cf86a55041c
PK <puneet.kittur@gmail.com>
2023-01-02 12:45:45 +0530

    Laptop changed to MacOS and deployed after that

--- mongo.js
+++ mongo.js
@@ -11,6 +11,8 @@ const password = process.argv[2];
 
 const url = `mongodb+srv://phonebookdb:${password}@cluster0.um2252f.mongodb.net/?retryWrites=true&w=majority`;
 
+// mongoose.set("strictQuery", true); This was added to check if the Deprecation error goes away, but there was no luck so commented this out
+
 const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
@@ -27,4 +29,3 @@ mongoose.connect(url).then(
     mongoose.connection.close();
   })
 );
-
