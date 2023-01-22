5cf99e78e55c7111ad76ff5b8694616fe8b3a7be
PK <puneet.kittur@gmail.com>
2023-01-22 23:10:30 +0530

    Made changes to few files to accomodate test db

--- utils/config.js
+++ utils/config.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 
 const { PORT } = process.env;
-// const { MONGODB_URI } = process.env;
+const { TEST_MONGODB_URI } = process.env;
 
-const MONGODB_URI =
-  process.env.NODE_ENV === "test"
-    ? process.env.TEST_MONGODB_URI
-    : process.env.MONGODB_URI;
+// const MONGODB_URI =
+//   process.env.NODE_ENV === "test"
+//     ? process.env.TEST_MONGODB_URI
+//     : process.env.MONGODB_URI;
 
 module.exports = {
-  MONGODB_URI,
+  TEST_MONGODB_URI,
   PORT,
 };
